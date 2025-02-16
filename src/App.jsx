66c84c18b1ca7a0afe6dc22bce075044b1a1d320
mollasisayjemere <mollasisayjemere@gmail.com>
2025-02-16 14:13:54 +0100

    Header parts

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
 import './App.css'
+import { Header } from './Header/Header'
 
 function App() {
   
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
      
-  Molla
+  <Header/>
      
      
     </>
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import {SILocationPin} from "react-icons/sl";
+import { FaSearch } from "react-icons/fa";
+import { BiCart } from "react-icons/bi";
+
 
 export const Header = () => {
   return (
@@ -7,10 +11,7 @@ export const Header = () => {
         <div>
           {/* logo */}
           <a href="/">
-            <img
-              src="../commenResoures/Amazon_icon.png"
-              alt="amazon logo"
-            />
+            <img src="../commenResoures/Amazon_icon.png" alt="amazon logo" />
           </a>
           {/* delivery */}
           <div></div>
@@ -29,14 +30,28 @@ export const Header = () => {
         {/* right side link */}
         <div>
           <div>
-            <img
-              src="../commenResoures/flag-us-svgrepo-com.svg"
-              alt=""
-            />
+            <img src="../commenResoures/flag-us-svgrepo-com.svg" alt="" />
             <section>
               <option value="">EN</option>
             </section>
           </div>
+          {/* three componewnts */}
+          <a href="">
+            <div>
+              <p>sign in</p>
+              <span>Account & List</span>
+            </div>
+          </a>
+          {/* order */}
+          <a href="">
+            <p>returns</p>
+            <span>& orders</span>
+          </a>
+          {/* Carts */}
+          <a to={ BiCart}>
+            {/* icon */}
+            <span>0</span>
+          </a>
         </div>
       </section>
     </section>
